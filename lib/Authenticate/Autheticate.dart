17421fbf70007cd0e1ad3488d04134072da6bf21
mrfachri19 <fachryfachry1997@gmail.com>
2023-07-27 13:34:48 +0700

    first

---
+++ lib/Authenticate/Autheticate.dart
@@ -0,0 +1,17 @@
+import 'package:chat_app_v2/Screens/HomeScreen.dart';
+import 'package:chat_app_v2/Authenticate/LoginScree.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+
+class Authenticate extends StatelessWidget {
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+
+  @override
+  Widget build(BuildContext context) {
+    if (_auth.currentUser != null) {
+      return HomeScreen();
+    } else {
+      return LoginScreen();
+    }
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:chat_app_v2/Authenticate/Autheticate.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'firebase_options.dart';
+
+Future main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Authenticate(),
+    );
+  }
+}
